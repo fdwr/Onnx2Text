cb3cd1e358b4b542866abd86dacf9d9c24583fd4
Dwayne Robinson <fdwr@hotmail.com>
2022-06-30 21:59:23 -0700

    Update zip package file list with Onnx2Text name

--- MakeZip.bat
+++ MakeZip.bat
@@ -1,1 +1,2 @@
-d:\programs\file\7-Zip\7z.exe a -tzip -mx=9 ./build/ConvertOnnxModel_%date%.zip -i@PackageFileList.txt %*
+del ./build/Onnx2Text.pikensoft.*.zip > nul
+\programs\file\7-Zip\7z.exe a -tzip -mx=9 ./build/Onnx2Text.pikensoft.%date%.zip -i@PackageFileList.txt %*
