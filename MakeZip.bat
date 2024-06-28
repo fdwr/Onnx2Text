14cb0a72585e2d39b49e292401d9c0b792358b7d
Dwayne Robinson <dwayner@microsoft.com>
2024-06-28 14:53:06 -0700

    Update MakeZip to launch releases page

--- MakeZip.bat
+++ MakeZip.bat
@@ -1,2 +1,3 @@
 del ./build/Onnx2Text.pikensoft.*.zip > nul
 \programs\file\7-Zip\7z.exe a -tzip -mx=9 ./build/Onnx2Text.pikensoft.%date%.zip -i@PackageFileList.txt %*
+start https://github.com/fdwr/Onnx2Text/releases
